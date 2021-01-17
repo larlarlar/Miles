6a6b689b0ddea7909052f6a8b3f05ac80f8d220f
Larisa Gurina <larisa.gurina.s@gmail.com>
2021-01-17 13:28:27 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 3_350_000;
+        int price = 3_250;
         int amount = 3;
-        int criterion = 2_000;
+        int criterion = 20;
 
                 // расчитываем бонус
         int bonus = price * amount / criterion ;
